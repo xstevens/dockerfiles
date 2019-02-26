35119329b2100d993f0acb618716c63936ccdc78
Xavier Stevens <xstevens@users.noreply.github.com>
2019-02-26 15:15:42 -0800

    Improved pyenv images to use non-root user and user level python modules

--- pyenv/python3/run.sh
+++ pyenv/python3/run.sh
@@ -2,7 +2,13 @@
 
 # install project dependencies
 if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
+    pip install -r requirements.txt --user
+fi
+
+# if binaries were installed in ~/.local/bin then lets add that directory to 
+# the path
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH=${PATH}:"$HOME/.local/bin"
 fi
 
 # drop into a bash shell
