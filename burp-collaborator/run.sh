96747d83c53efbb5576ad6eb14d24662f668e766
Xavier Stevens <xstevens@users.noreply.github.com>
2024-01-16 09:52:56 -0800

    Sync latest changes to a few containers

--- burp-collaborator/run.sh
+++ burp-collaborator/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 # env vars that should be set by docker run
-export BURP_SERVER_DOMAIN=${BURP_SERVER_DOMAIN:-"burpcollaborator.ecorp.com"}
+export BURP_SERVER_DOMAIN=${BURP_SERVER_DOMAIN:-"burpcollaborator.example.com"}
 export BURP_SSL_DOMAIN=${BURP_SSL_DOMAIN:-$SERVER_DOMAIN}
 export BURP_METRICS_PATH=${BURP_METRICS_PATH:-"ae6bf3b062d2492fb9cf7579781271f6"}
 
--- puppeteer/screenshot.js
+++ puppeteer/screenshot.js
@@ -2,14 +2,14 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser',
+    //executablePath: '/usr/bin/chromium-browser',
     headless: true,
     args: ['--disable-dev-shm-usage'],
   });
   const page = await browser.newPage();
-  await page.setViewport({width: 1280, height: 1440});
   let url = process.env.TARGET_URL;
   try {
+    await page.setViewport({width: 1280, height: 1440});
     await page.goto(url, {timeout: 30000});
     await page.screenshot({path: '/screenshots/screenshot.png'});
   } catch (error) {
